5da0de584e886371b6bb48f8af90663fc1e5b1b9
quangdangcodegym <quang.dang@codegym.vn>
2023-04-07 10:39:08 +0700

    hello java

---
+++ HelloJava/src/main/java/com/codegym/Customer.java
@@ -0,0 +1,9 @@
+package com.codegym;
+
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("Hello C0223G1");
+
+        
+    }
+}
---
+++ HelloJava/src/main/java/com/codegym/Product.java
@@ -0,0 +1,61 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Product {
+    public static Scanner scanner = new Scanner(System.in);
+
+    // gõ: main
+    // gõ: sout: dung để in ra màn hình
+    public static void main(String[] args) {
+        /**
+        System.out.println("Hello C0223G1");
+
+        System.out.println("Nhập vào 1 chuoi:");
+        String b = scanner.nextLine();
+        System.out.println(b);
+
+        System.out.println("Nhập vào 1  số:");
+        int a = scanner.nextInt();
+
+        System.out.println(a);
+
+
+         // let a = 5;
+         int a = 130;
+         float c = 0.0f;
+         double d = 0.0;
+
+
+         int a =6;
+         int b = a++ + ++a*2;
+         System.out.println(a);
+         System.out.println(b);
+
+
+         System.out.println("Nhập vào 1 số:  ");
+         int number = scanner.nextInt();
+
+         if (number > 0) {
+             for (int i = 1; i < number; i++) {
+             if (i % 2 == 0) {
+                System.out.println(i);
+                }
+            }
+         }else{
+            System.out.println("Số nhap vào phải lớn hơn 1");
+         }
+
+
+         // printf với kí tự xuống dòng \n
+         int number;
+         do{
+             System.out.printf("Nhap vao 1 so \n");
+             number  = scanner.nextInt();
+         }while (number<0);
+         **/
+
+
+
+    }
+}
